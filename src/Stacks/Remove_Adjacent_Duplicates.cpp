8e0f510c508d684fe15819e739074f2f78da0d65
Rahul <rahulchityala1998@gmail.com>
2026-06-23 20:28:45 +0530

    Added Remove Adjacent Duplicates

--- src/Stacks/Remove_Adjacent_Duplicates.cpp
+++ src/Stacks/Remove_Adjacent_Duplicates.cpp
@@ -1,26 +1,44 @@
+/*This is done just for practice same thing could be implemented using simple string data structure*/
+
 #include <iostream>
 #include <stack>
 using namespace std;
-stack<char> removeAdjDup(char arr[],int size){
+stack<char> removeAdjDup(string str){
 	stack<char> Stack;
-	for (int k = 0; k < size; ++k) {
-		if(Stack.top() == arr[k]){
+	for (int k = 0; k < str.size(); ++k) {
+		if(Stack.empty()){
+			Stack.push(str[k]);
+			continue;
+		}
+		if(Stack.top() == str[k]){
 			Stack.pop();
 		}else{
-			Stack.push(arr[k]);
+			Stack.push(str[k]);
 		}
 	}
 
 	return Stack;
 }
 int main(){
-	char arr[5];
-	for (int i = 0; i < 5; ++i) {
-		cin>> arr[i];
-	}
-	stack<char> st = removeAdjDup(arr,5);
+	string str;
+
+		cin>> str;
+string res ="";
+	stack<char> st = removeAdjDup(str);
 	while(!st.empty()){
-		  cout << st.top() << " ";
-		    st.pop();
+		res +=st.top();
+		st.pop();
 	}
+	// reversing the string
+	int left =0;
+	int right = res.size()-1;
+	while(left <= right){
+		int temp = res[left];
+		res[left] = res[right];
+		res[right] = temp;
+
+		left++;
+		right--;
+	}
+	cout<<res;
 }
