663b73fbb4100fd18643cacb8a09c500e8f94c8e
Rahul <rahulchityala1998@gmail.com>
2026-07-02 00:48:49 +0530

    Added next greater element to Arrays

---
+++ src/Arrays/next_greater_element.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int arr[] = {2, 1, 2, 4, 3};
+	int res[5] ={};
+	int size = sizeof(arr)/sizeof(arr[0]);
+	int write = 0;
+	int scanner = 1;
+
+	while(write < size){
+		if(scanner >= size){
+			res[write] = -1;
+			continue;
+		}
+		if(arr[scanner] > arr[write]){
+			res[write] = arr[scanner];
+			write++;
+			scanner = write+1;
+		}else if(scanner == size-1){
+			res[write] = -1;
+			write++;
+			scanner = write+1;
+		}else{
+			scanner++;
+		}
+	}
+	for (int i = 0; i < 5; ++i) {
+cout<< res[i] << ",";
+	}
+}
